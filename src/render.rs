7060c7270fd38932820740fee8f416146314700f
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-02-17 01:32:25 -0300

    Change terminal colors

--- src/render.rs
+++ src/render.rs
@@ -7,7 +7,7 @@ use std::io::{Stdout, Write};
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, current_frame: &Frame, force: bool) {
     if force {
-        stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::DarkGrey)).unwrap();
         stdout.queue(Clear(ClearType::All)).unwrap();
         stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
     }
